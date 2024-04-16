63912ebcdd5a0fb95522225847ac1428e18b5804
Robson Cerqueira <robsonwebpro@gmail.com>
2024-04-15 21:51:22 -0300

    primeiro commit

---
+++ cypress/support/commands.js
@@ -0,0 +1,11 @@
+Cypress.Commands.add('postUser', (user) => {
+    cy.api({
+        url: '/users',
+        method: 'POST',
+        body: user,
+        failOnStatusCode: false,
+    }).then(response => {
+        return response
+    })
+
+})
